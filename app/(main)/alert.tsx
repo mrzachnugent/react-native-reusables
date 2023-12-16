762dfce1a35b7ab9e3182217af0a76ddc59ebf5d
Zach Nugent <mrzachnugent@gmail.com>
2023-12-16 16:35:20 -0500

    add alert

--- app/(main)/alert.tsx
+++ app/(main)/alert.tsx
@@ -1,10 +1,29 @@
 import { View } from 'react-native';
-import { Alert } from '~/components/ui/alert';
+import { Alert, AlertDescription, AlertTitle } from '~/components/ui/alert';
 
 export default function AlertScreen() {
   return (
-    <View className='flex-1 justify-center items-center'>
-      <Alert />
+    <View className='flex-1 justify-center p-6 items-center gap-6'>
+      <Alert icon='Terminal'>
+        <AlertTitle>Heads up!</AlertTitle>
+        <AlertDescription>
+          You can use a terminal to run commands on your computer.
+        </AlertDescription>
+      </Alert>
+      <Alert icon='AlertTriangle' variant='destructive'>
+        <AlertTitle>Danger!</AlertTitle>
+        <AlertDescription>
+          High voltage. Do not touch. Risk of electric shock. Keep away from
+          children.
+        </AlertDescription>
+      </Alert>
+      <Alert icon='CheckSquare' variant='success'>
+        <AlertTitle>Success!</AlertTitle>
+        <AlertDescription>
+          You have successfully completed the tutorial. You can now go touch
+          some grass.
+        </AlertDescription>
+      </Alert>
     </View>
   );
 }
--- components/ui/alert.tsx
+++ components/ui/alert.tsx
@@ -1,5 +1,85 @@
-import { Text } from 'react-native';
+import * as React from 'react';
+import { cva, type VariantProps } from 'class-variance-authority';
 
-export function Alert() {
-  return <Text className='text-foreground'>Alert</Text>;
-}
+import { cn } from '~/lib/utils';
+import { View, Text } from 'react-native';
+import * as LucideIcon from 'lucide-react-native';
+
+const alertVariants = cva(
+  'bg-background relative w-full rounded-lg border p-5',
+  {
+    variants: {
+      variant: {
+        default: 'border-muted-foreground',
+        destructive: 'border-destructive',
+        success: 'border-emerald-500',
+      },
+    },
+    defaultVariants: {
+      variant: 'default',
+    },
+  }
+);
+
+const Alert = React.forwardRef<
+  React.ElementRef<typeof View>,
+  React.ComponentPropsWithoutRef<typeof View> &
+    VariantProps<typeof alertVariants> & {
+      icon?: keyof typeof LucideIcon;
+    }
+>(({ children, icon, className, variant, ...props }, ref) => {
+  const Icon = LucideIcon[icon ?? 'AlertTriangle'] as LucideIcon.Icon;
+  return (
+    <View
+      ref={ref}
+      role='alert'
+      className={cn(alertVariants({ variant }), icon && 'pl-12', className)}
+      {...props}
+    >
+      {icon && (
+        <Icon
+          size={18}
+          className={cn(
+            variant === 'destructive'
+              ? 'text-destructive'
+              : variant === 'success'
+              ? 'text-emerald-500'
+              : 'text-foreground',
+            'translate-y-[-3px] absolute left-3.5 top-[19]'
+          )}
+        />
+      )}
+      {children}
+    </View>
+  );
+});
+Alert.displayName = 'Alert';
+
+const AlertTitle = React.forwardRef<
+  React.ElementRef<typeof Text>,
+  React.ComponentPropsWithoutRef<typeof Text>
+>(({ className, ...props }, ref) => (
+  <Text
+    ref={ref}
+    className={cn(
+      'mb-1 text-lg font-medium leading-none tracking-tight text-foreground',
+      className
+    )}
+    {...props}
+  />
+));
+AlertTitle.displayName = 'AlertTitle';
+
+const AlertDescription = React.forwardRef<
+  React.ElementRef<typeof Text>,
+  React.ComponentPropsWithoutRef<typeof Text>
+>(({ className, ...props }, ref) => (
+  <Text
+    ref={ref}
+    className={cn('text-secondary-foreground', className)}
+    {...props}
+  />
+));
+AlertDescription.displayName = 'AlertDescription';
+
+export { Alert, AlertTitle, AlertDescription };
