a9ee2e3274aeef0e6a9aed9b9ab8d8b75c01991b
Zach Nugent <mrzachnugent@gmail.com>
2023-12-16 21:49:39 -0500

    add card

--- app/(main)/card.tsx
+++ app/(main)/card.tsx
@@ -1,10 +1,28 @@
-import { View } from 'react-native';
-import { Card } from '~/components/ui/card';
+import { View, Text } from 'react-native';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '~/components/ui/card';
 
 export default function CardScreen() {
   return (
-    <View className='flex-1 justify-center items-center'>
-      <Card />
+    <View className='flex-1 justify-center p-6'>
+      <Card>
+        <CardHeader>
+          <CardTitle>Card Title</CardTitle>
+          <CardDescription>Card Description</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <Text className='text-foreground text-xl'>Card Content</Text>
+        </CardContent>
+        <CardFooter>
+          <Text className='text-muted-foreground'>Card Footer</Text>
+        </CardFooter>
+      </Card>
     </View>
   );
 }
--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -1,5 +1,112 @@
-import { Text } from 'react-native';
+import { useColorScheme } from 'nativewind';
+import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
 
-export function Card() {
-  return <Text className='text-foreground'>Card</Text>;
-}
+import { cn } from '~/lib/utils';
+
+const Card = React.forwardRef<
+  React.ElementRef<typeof View>,
+  React.ComponentPropsWithoutRef<typeof View>
+>(({ className, ...props }, ref) => {
+  const { colorScheme } = useColorScheme();
+  return (
+    <View
+      ref={ref}
+      className={cn(
+        'rounded-lg border border-border bg-card px-0.5 py-2',
+        className
+      )}
+      style={colorScheme === 'dark' ? styles.shadowDark : styles.shadowLight}
+      {...props}
+    />
+  );
+});
+
+Card.displayName = 'Card';
+
+const CardHeader = React.forwardRef<
+  React.ElementRef<typeof View>,
+  React.ComponentPropsWithoutRef<typeof View>
+>(({ className, ...props }, ref) => (
+  <View ref={ref} className={cn('gap-1.5 p-6', className)} {...props} />
+));
+CardHeader.displayName = 'CardHeader';
+
+const CardTitle = React.forwardRef<
+  React.ElementRef<typeof Text>,
+  React.ComponentPropsWithoutRef<typeof Text>
+>(({ className, ...props }, ref) => (
+  <Text
+    ref={ref}
+    className={cn(
+      'text-3xl font-semibold leading-none tracking-tight text-foreground',
+      className
+    )}
+    {...props}
+  />
+));
+CardTitle.displayName = 'CardTitle';
+
+const CardDescription = React.forwardRef<
+  React.ElementRef<typeof Text>,
+  React.ComponentPropsWithoutRef<typeof Text>
+>(({ className, ...props }, ref) => (
+  <Text
+    ref={ref}
+    className={cn('text-base text-muted-foreground', className)}
+    {...props}
+  />
+));
+CardDescription.displayName = 'CardDescription';
+
+const CardContent = React.forwardRef<
+  React.ElementRef<typeof View>,
+  React.ComponentPropsWithoutRef<typeof View>
+>(({ className, ...props }, ref) => (
+  <View ref={ref} className={cn('p-6 pt-0', className)} {...props} />
+));
+CardContent.displayName = 'CardContent';
+
+const CardFooter = React.forwardRef<
+  React.ElementRef<typeof View>,
+  React.ComponentPropsWithoutRef<typeof View>
+>(({ className, ...props }, ref) => (
+  <View
+    ref={ref}
+    className={cn('flex items-center p-6 pt-0', className)}
+    {...props}
+  />
+));
+CardFooter.displayName = 'CardFooter';
+
+export {
+  Card,
+  CardHeader,
+  CardFooter,
+  CardTitle,
+  CardDescription,
+  CardContent,
+};
+
+const styles = StyleSheet.create({
+  shadowLight: {
+    shadowColor: '#000000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.05,
+    shadowRadius: 8,
+    elevation: 2,
+  },
+  shadowDark: {
+    shadowColor: '#FFFFFF',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.05,
+    shadowRadius: 8,
+    elevation: 2,
+  },
+});
