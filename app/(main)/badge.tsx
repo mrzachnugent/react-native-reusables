24cae6b4bb244181632c280fa363ed1af0a7785f
Zach Nugent <mrzachnugent@gmail.com>
2023-12-16 19:43:21 -0500

    add badge

--- app/(main)/badge.tsx
+++ app/(main)/badge.tsx
@@ -3,8 +3,11 @@ import { Badge } from '~/components/ui/badge';
 
 export default function BadgeScreen() {
   return (
-    <View className='flex-1 justify-center items-center'>
-      <Badge />
+    <View className='flex-1 justify-center items-center gap-5'>
+      <Badge>Badge</Badge>
+      <Badge variant={'secondary'}>Badge</Badge>
+      <Badge variant={'destructive'}>Badge</Badge>
+      <Badge variant={'outline'}>Badge</Badge>
     </View>
   );
 }
--- components/ui/badge.tsx
+++ components/ui/badge.tsx
@@ -1,5 +1,56 @@
-import { Text } from 'react-native';
+import * as React from 'react';
+import { cva, type VariantProps } from 'class-variance-authority';
 
-export function Badge() {
-  return <Text className='text-foreground'>Badge</Text>;
-}
+import { cn } from '~/lib/utils';
+import { View, Text } from 'react-native';
+
+const badgeRootVariants = cva(
+  'inline-flex items-center rounded-full border px-2.5 py-0.5',
+  {
+    variants: {
+      variant: {
+        default: 'border-transparent bg-primary ',
+        secondary: 'border-transparent bg-secondary ',
+        destructive: 'border-transparent bg-destructive ',
+        outline: 'border-border',
+      },
+    },
+    defaultVariants: {
+      variant: 'default',
+    },
+  }
+);
+
+const badgeTextVariants = cva('text-sm font-semibold', {
+  variants: {
+    variant: {
+      default: 'text-primary-foreground',
+      secondary: 'text-secondary-foreground',
+      destructive: 'text-destructive-foreground',
+      outline: 'text-foreground',
+    },
+  },
+  defaultVariants: {
+    variant: 'default',
+  },
+});
+
+const Badge = React.forwardRef<
+  React.ElementRef<typeof View>,
+  React.ComponentPropsWithoutRef<typeof View> &
+    VariantProps<typeof badgeRootVariants> & { textClass?: string }
+>(({ className, children, textClass, variant, ...props }, ref) => {
+  return (
+    <View
+      ref={ref}
+      className={cn(badgeRootVariants({ variant }), className)}
+      {...props}
+    >
+      <Text className={cn(badgeTextVariants({ variant }), textClass)}>
+        {children}
+      </Text>
+    </View>
+  );
+});
+
+export { Badge, badgeRootVariants, badgeTextVariants };
