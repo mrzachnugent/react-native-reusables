97637510c3d1d8f4661fd2cffe1ac0dcccbd0356
Zach Nugent <mrzachnugent@gmail.com>
2023-12-18 15:28:13 -0500

    add progress bar

--- components/ui/progress.tsx
+++ components/ui/progress.tsx
@@ -1,5 +1,55 @@
-import { Text } from 'react-native';
+import React from 'react';
+import { View } from 'react-native';
+import Animated, {
+  Extrapolate,
+  SharedValue,
+  interpolate,
+  useAnimatedStyle,
+  withSpring,
+} from 'react-native-reanimated';
+import { cn } from '~/lib/utils';
 
-export function Progress() {
-  return <Text className='text-foreground'>Progress</Text>;
-}
+const Progress = React.forwardRef<
+  React.ElementRef<typeof Animated.View>,
+  React.ComponentPropsWithoutRef<typeof Animated.View> & {
+    /**
+     * Value between 0 and 100
+     */
+    progress: SharedValue<number>;
+    rootClass?: string;
+  }
+>(({ progress, rootClass, className, style, ...props }, ref) => {
+  const stylez = useAnimatedStyle(() => {
+    return {
+      width: withSpring(
+        `${interpolate(
+          progress.value,
+          [0, 100],
+          [1, 100],
+          Extrapolate.CLAMP
+        )}%`,
+        { overshootClamping: true }
+      ),
+    };
+  });
+
+  return (
+    <View
+      className={cn(
+        'rounded-xl h-4 bg-border overflow-hidden relative',
+        rootClass
+      )}
+      role='progressbar'
+    >
+      <Animated.View
+        style={[stylez, style]}
+        className={cn('h-full bg-foreground', className)}
+        {...props}
+      />
+    </View>
+  );
+});
+
+Progress.displayName = 'Progress';
+
+export { Progress };
