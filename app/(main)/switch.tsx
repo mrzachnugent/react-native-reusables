f854b2db1b1bd629634f5b02d73bec99ba657fad
Zach Nugent <mrzachnugent@gmail.com>
2023-12-17 22:29:21 -0500

    add switch

--- app/(main)/switch.tsx
+++ app/(main)/switch.tsx
@@ -1,10 +1,28 @@
+import React from 'react';
 import { View } from 'react-native';
+import { Label } from '~/components/ui/label';
 import { Switch } from '~/components/ui/switch';
 
 export default function SwitchScreen() {
+  const [value, setValue] = React.useState(false);
   return (
     <View className='flex-1 justify-center items-center'>
-      <Switch />
+      <View className='flex-row items-center gap-5'>
+        <Switch
+          accessibilityLabelledBy='switchLabel'
+          value={value}
+          onValueChange={setValue}
+        />
+        <Label
+          onPress={() => {
+            setValue((prev) => !prev);
+          }}
+          nativeID='switchLabel'
+          className='text-xl pb-2'
+        >
+          Airplane mode
+        </Label>
+      </View>
     </View>
   );
 }
