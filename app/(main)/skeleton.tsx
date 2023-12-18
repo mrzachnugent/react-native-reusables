ab06b5b2a63f32cd82d7e5af2fb2a0cf066bfdb3
Zach Nugent <mrzachnugent@gmail.com>
2023-12-18 12:30:04 -0500

    add skeleton

--- app/(main)/skeleton.tsx
+++ app/(main)/skeleton.tsx
@@ -1,10 +1,42 @@
+import React from 'react';
 import { View } from 'react-native';
+import { Button } from '~/components/ui/button';
 import { Skeleton } from '~/components/ui/skeleton';
 
 export default function SkeletonScreen() {
+  const [isLoading, setIsLoading] = React.useState(false);
+
+  React.useEffect(() => {
+    let timeout: ReturnType<typeof setTimeout> | null = null;
+    if (isLoading) {
+      timeout = setTimeout(() => {
+        setIsLoading(false);
+      }, 3000);
+    }
+    return () => {
+      if (timeout) {
+        clearTimeout(timeout);
+      }
+    };
+  }, [isLoading]);
+
   return (
     <View className='flex-1 justify-center items-center'>
-      <Skeleton />
+      <Skeleton
+        key={`skeleton-${isLoading}`} // key is not needed if loading only goes one way (not toggled)
+        show={isLoading}
+        radius={5}
+        width={200}
+      >
+        <Button
+          disabled={isLoading}
+          onPress={() => {
+            setIsLoading(true);
+          }}
+        >
+          Show Loading
+        </Button>
+      </Skeleton>
     </View>
   );
 }
--- components/ui/skeleton.tsx
+++ components/ui/skeleton.tsx
@@ -1,5 +1,32 @@
-import { Text } from 'react-native';
-
-export function Skeleton() {
-  return <Text className='text-foreground'>Skeleton</Text>;
+import { Skeleton as MotiSkeleton } from 'moti/skeleton';
+import { useColorScheme } from 'nativewind';
+import React from 'react';
+import { NAV_THEME } from '~/lib/constants';
+const COLORS_LIGHT_THEME = [
+  NAV_THEME.light.card,
+  NAV_THEME.light.border,
+  NAV_THEME.light.card,
+  NAV_THEME.light.border,
+  NAV_THEME.light.card,
+  NAV_THEME.light.border,
+];
+const COLORS_DARK_THEME = [
+  NAV_THEME.dark.border,
+  NAV_THEME.dark.card,
+  NAV_THEME.dark.border,
+  NAV_THEME.dark.card,
+  NAV_THEME.dark.card,
+  NAV_THEME.dark.border,
+];
+/**
+ * @docs https://moti.fyi/skeleton
+ */
+export function Skeleton(props: React.ComponentProps<typeof MotiSkeleton>) {
+  const { colorScheme } = useColorScheme();
+  const {
+    colorMode = colorScheme,
+    colors = colorScheme === 'dark' ? COLORS_DARK_THEME : COLORS_LIGHT_THEME,
+    ...rest
+  } = props;
+  return <MotiSkeleton colorMode={colorMode} colors={colors} {...rest} />;
 }
