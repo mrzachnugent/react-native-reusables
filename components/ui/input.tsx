589e579f38c425d2176bb4af07683ae757dfd463
Zach Nugent <mrzachnugent@gmail.com>
2023-12-17 19:12:31 -0500

    add input + label

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -1,5 +1,25 @@
-import { Text } from 'react-native';
+import React from 'react';
+import { TextInput } from 'react-native';
 
-export function Input() {
-  return <Text className='text-foreground'>Input</Text>;
-}
+import { cn } from '~/lib/utils';
+
+const Input = React.forwardRef<
+  React.ElementRef<typeof TextInput>,
+  React.ComponentPropsWithoutRef<typeof TextInput>
+>(({ className, placeholderClassName, ...props }, ref) => {
+  return (
+    <TextInput
+      ref={ref}
+      className={cn(
+        'rounded-md border border-input bg-muted px-3 text-lg h-12 leading-6 text-foreground items-center  placeholder:text-muted-foreground disabled:opacity-50',
+        className
+      )}
+      placeholderClassName={cn('text-muted-foreground', placeholderClassName)}
+      {...props}
+    />
+  );
+});
+
+Input.displayName = 'Input';
+
+export { Input };
--- components/ui/label.tsx
+++ components/ui/label.tsx
@@ -1,5 +1,29 @@
-import { Text } from 'react-native';
+import React from 'react';
 
-export function Label() {
-  return <Text className='text-foreground'>Label</Text>;
-}
+import { cn } from '~/lib/utils';
+import { Text, Pressable } from 'react-native';
+
+const Label = React.forwardRef<
+  React.ElementRef<typeof Text>,
+  React.ComponentPropsWithoutRef<typeof Text> & {
+    rootProps?: Omit<
+      React.ComponentPropsWithoutRef<typeof Pressable>,
+      'onPress'
+    >;
+  }
+>(({ className, onPress, rootProps, ...props }, ref) => (
+  <Pressable onPress={onPress} {...rootProps}>
+    <Text
+      ref={ref}
+      className={cn(
+        'text-lg text-foreground font-medium px-0.5 py-1.5 leading-none',
+        className
+      )}
+      {...props}
+    />
+  </Pressable>
+));
+
+Label.displayName = 'Label';
+
+export { Label };
