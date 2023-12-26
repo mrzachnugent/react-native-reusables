f0448fda5e6832366c5ab419a660e6a37b66ed6d
Zach Nugent <mrzachnugent@gmail.com>
2023-12-26 14:22:29 -0500

    add form poc

--- app/(main)/form.tsx
+++ app/(main)/form.tsx
@@ -1,10 +1,85 @@
-import { View } from 'react-native';
-import { Form } from '~/components/ui/form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { Alert, View } from 'react-native';
+import * as z from 'zod';
+import { Button } from '~/components/ui/button';
+import { Form, FormField, FormInput } from '~/components/ui/form';
+
+const formSchema = z.object({
+  email: z.string().email({
+    message: 'Please enter a valid email address.',
+  }),
+  username: z.string().min(2, {
+    message: 'Username must be at least 2 characters.',
+  }),
+  password: z.string().min(8, {
+    message: 'Password must be at least 8 characters.',
+  }),
+});
 
 export default function FormScreen() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: '',
+      username: '',
+      password: '',
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    Alert.alert('Submitted!', JSON.stringify(values, null, 2));
+    form.reset();
+  }
+
   return (
-    <View className='flex-1 justify-center items-center'>
-      <Form />
+    <View className='flex-1 justify-center p-6'>
+      <Form {...form}>
+        <View className='gap-6'>
+          <FormField
+            control={form.control}
+            name='email'
+            render={({ field }) => (
+              <FormInput
+                label='Email'
+                placeholder='hello@zachnugent.ca'
+                autoCapitalize='none'
+                autoComplete='email'
+                {...field}
+              />
+            )}
+          />
+          <FormField
+            control={form.control}
+            name='username'
+            render={({ field }) => (
+              <FormInput
+                label='Username'
+                placeholder='mrzachnugent'
+                description='This is your public display name.'
+                autoCapitalize='none'
+                {...field}
+              />
+            )}
+          />
+          <FormField
+            control={form.control}
+            name='password'
+            render={({ field }) => (
+              <FormInput
+                label='Password'
+                placeholder='********'
+                description='Use a secure password.'
+                secureTextEntry
+                autoComplete='password'
+                {...field}
+              />
+            )}
+          />
+          <View />
+          <Button onPress={form.handleSubmit(onSubmit)}>Submit</Button>
+        </View>
+      </Form>
     </View>
   );
 }
--- components/ui/form.tsx
+++ components/ui/form.tsx
@@ -1,6 +1,232 @@
-import { Text } from 'react-native';
+// This project uses code from shadcn/ui.
+// The code is licensed under the MIT License.
+// https://github.com/shadcn-ui/ui
 
-// TODO: https://react-hook-form.com/get-started#ReactNative
-export function Form() {
-  return <Text className='text-foreground'>Form</Text>;
-}
+import React from 'react';
+import {
+  Controller,
+  ControllerProps,
+  FieldPath,
+  FieldValues,
+  FormProvider,
+  useFormContext,
+} from 'react-hook-form';
+import { Text, View } from 'react-native';
+
+import Animated, { FadeInDown, FadeOutUp } from 'react-native-reanimated';
+import { Input } from '~/components/ui/input';
+import { Label } from '~/components/ui/label';
+import { cn } from '~/lib/utils';
+
+const Form = FormProvider;
+
+type FormFieldContextValue<
+  TFieldValues extends FieldValues = FieldValues,
+  TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>
+> = {
+  name: TName;
+};
+
+const FormFieldContext = React.createContext<FormFieldContextValue>(
+  {} as FormFieldContextValue
+);
+
+const FormField = <
+  TFieldValues extends FieldValues = FieldValues,
+  TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>
+>({
+  ...props
+}: ControllerProps<TFieldValues, TName>) => {
+  return (
+    <FormFieldContext.Provider value={{ name: props.name }}>
+      <Controller {...props} />
+    </FormFieldContext.Provider>
+  );
+};
+
+const useFormField = () => {
+  const fieldContext = React.useContext(FormFieldContext);
+  const itemContext = React.useContext(FormItemContext);
+  const { getFieldState, formState, handleSubmit } = useFormContext();
+
+  const fieldState = getFieldState(fieldContext.name, formState);
+
+  if (!fieldContext) {
+    throw new Error('useFormField should be used within <FormField>');
+  }
+
+  const { nativeID } = itemContext;
+
+  return {
+    nativeID,
+    name: fieldContext.name,
+    formItemNativeID: `${nativeID}-form-item`,
+    formDescriptionNativeID: `${nativeID}-form-item-description`,
+    formMessageNativeID: `${nativeID}-form-item-message`,
+    handleSubmit,
+    ...fieldState,
+  };
+};
+
+type FormItemContextValue = {
+  nativeID: string;
+};
+
+const FormItemContext = React.createContext<FormItemContextValue>(
+  {} as FormItemContextValue
+);
+
+const FormItem = React.forwardRef<
+  React.ElementRef<typeof View>,
+  React.ComponentPropsWithoutRef<typeof View>
+>(({ className, ...props }, ref) => {
+  const nativeID = React.useId();
+
+  return (
+    <FormItemContext.Provider value={{ nativeID }}>
+      <View ref={ref} className={cn('space-y-2', className)} {...props} />
+    </FormItemContext.Provider>
+  );
+});
+FormItem.displayName = 'FormItem';
+
+const FormLabel = React.forwardRef<
+  React.ElementRef<typeof Label>,
+  React.ComponentPropsWithoutRef<typeof Label>
+>(({ className, ...props }, ref) => {
+  const { error, formItemNativeID } = useFormField();
+
+  return (
+    <Label
+      ref={ref}
+      className={cn(error && 'text-destructive', className)}
+      nativeID={formItemNativeID}
+      {...props}
+    />
+  );
+});
+FormLabel.displayName = 'FormLabel';
+
+const FormDescription = React.forwardRef<
+  React.ElementRef<typeof Text>,
+  React.ComponentPropsWithoutRef<typeof Text>
+>(({ className, ...props }, ref) => {
+  const { formDescriptionNativeID } = useFormField();
+
+  return (
+    <Text
+      ref={ref}
+      id={formDescriptionNativeID}
+      className={cn('text-sm text-muted-foreground', className)}
+      {...props}
+    />
+  );
+});
+FormDescription.displayName = 'FormDescription';
+
+const FormMessage = React.forwardRef<
+  React.ElementRef<typeof Animated.Text>,
+  React.ComponentPropsWithoutRef<typeof Animated.Text>
+>(({ className, children, ...props }, ref) => {
+  const { error, formMessageNativeID } = useFormField();
+  const body = error ? String(error?.message) : children;
+
+  if (!body) {
+    return null;
+  }
+
+  return (
+    <Animated.Text
+      entering={FadeInDown}
+      exiting={FadeOutUp.duration(275)}
+      ref={ref}
+      id={formMessageNativeID}
+      className={cn('text-sm font-medium text-destructive', className)}
+      {...props}
+    >
+      {body}
+    </Animated.Text>
+  );
+});
+FormMessage.displayName = 'FormMessage';
+
+const FormInput = React.forwardRef<
+  React.ElementRef<typeof Input>,
+  React.ComponentPropsWithoutRef<typeof Input> & {
+    label?: string;
+    description?: string;
+  }
+>(({ label, description, onChange, ...props }, ref) => {
+  const inputRef = React.useRef<React.ComponentRef<typeof Input>>(null);
+  const {
+    error,
+    formItemNativeID,
+    formDescriptionNativeID,
+    formMessageNativeID,
+  } = useFormField();
+
+  React.useImperativeHandle(
+    ref,
+    () => {
+      if (!inputRef.current) {
+        return {} as React.ComponentRef<typeof Input>;
+      }
+      return inputRef.current;
+    },
+    [inputRef.current]
+  );
+
+  function handleOnLabelPress() {
+    if (!inputRef.current) {
+      return;
+    }
+    if (inputRef.current.isFocused()) {
+      inputRef.current?.blur();
+    } else {
+      inputRef.current?.focus();
+    }
+  }
+
+  return (
+    <FormItem>
+      {!!label && <FormLabel onPress={handleOnLabelPress}>{label}</FormLabel>}
+
+      <Input
+        ref={inputRef}
+        nativeID={formItemNativeID}
+        aria-describedby={
+          !error
+            ? `${formDescriptionNativeID}`
+            : `${formDescriptionNativeID} ${formMessageNativeID}`
+        }
+        aria-invalid={!!error}
+        onChangeText={onChange as ((text: string) => void) | undefined}
+        {...props}
+      />
+      {!!description && <FormDescription>{description}</FormDescription>}
+      <FormMessage />
+    </FormItem>
+  );
+});
+
+FormInput.displayName = 'FormInput';
+
+// TODO: add Form components for:
+// Checkbox
+// Date Picker
+// Radio Group
+// Select
+// Switch
+// Textarea
+// Combobox
+
+export {
+  Form,
+  FormDescription,
+  FormField,
+  FormInput,
+  FormItem,
+  FormLabel,
+  FormMessage,
+  useFormField,
+};
