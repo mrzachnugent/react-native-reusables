19899a42ceab873fbf1f68e9b9a6f868ede9f4fc
the-simian <harlinjesse@gmail.com>
2024-02-05 22:10:53 -0600

    adds the css fix that corrects the popover demo

--- app/(main)/popover/index.tsx
+++ app/(main)/popover/index.tsx
@@ -1,36 +1,36 @@
-import React from 'react';
-import { Text, View, ScrollView, Dimensions } from 'react-native';
-import { Alert, AlertDescription, AlertTitle } from '~/components/ui/alert';
-import { buttonTextVariants } from '~/components/ui/button';
-import { Input } from '~/components/ui/input';
-import { Label } from '~/components/ui/label';
+import React from "react";
+import { Text, View, ScrollView, Dimensions } from "react-native";
+import { Alert, AlertDescription, AlertTitle } from "~/components/ui/alert";
+import { buttonTextVariants } from "~/components/ui/button";
+import { Input } from "~/components/ui/input";
+import { Label } from "~/components/ui/label";
 import {
   Popover,
   PopoverContent,
   PopoverTrigger,
-} from '~/components/ui/popover';
-import { cn } from '~/lib/utils';
+} from "~/components/ui/popover";
+import { cn } from "~/lib/utils";
 
-const { height } = Dimensions.get('window');
+const { height } = Dimensions.get("window");
 
 export default function PopoverScreen() {
   return (
     <ScrollView>
-      <View className='p-6 w-full'>
-        <Alert icon='Code' className='max-w-xl'>
+      <View className="w-full p-6">
+        <Alert icon="Code" className="max-w-xl">
           <AlertTitle>FYI</AlertTitle>
           <AlertDescription>
             This reusable does not use "rn-primitives"
           </AlertDescription>
         </Alert>
       </View>
-      <View style={{ height: height / 3 }} className='pt-4 p-6'>
+      <View style={{ height: height / 3 }} className="p-6 pt-4">
         <Popover>
-          <PopoverTrigger className='w-full'>
+          <PopoverTrigger className="w-full">
             {({ pressed }) => (
               <Text
                 className={cn(
-                  pressed ? 'opacity-70' : '',
+                  pressed ? "opacity-70" : "",
                   buttonTextVariants({})
                 )}
               >
@@ -38,88 +38,88 @@ export default function PopoverScreen() {
               </Text>
             )}
           </PopoverTrigger>
-          <PopoverContent className='gap-6 ' position='bottom'>
+          <PopoverContent className="gap-6" position="bottom">
             <View>
-              <Text className='text-2xl font-bold text-foreground'>
+              <Text className="text-2xl font-bold text-foreground">
                 Dimensions
               </Text>
-              <Text className='text-lg text-muted-foreground'>
+              <Text className="text-lg text-muted-foreground">
                 Set the dimensions for the layer.
               </Text>
             </View>
-            <View className='flex-row gap-5 items-center'>
-              <Label className='pt-3 font-semibold w-20'>Width:</Label>
-              <Input className='flex-1' defaultValue='100%' />
+            <View className="flex-row items-center gap-5">
+              <Label className="w-20 pt-3 font-semibold">Width:</Label>
+              <Input className="flex-1 min-w-0" defaultValue="100%" />
             </View>
-            <View className='flex-row gap-5 items-center'>
-              <Label className='pt-3 font-semibold w-20'>Height:</Label>
-              <Input className='flex-1' defaultValue='25px' />
+            <View className="flex-row items-center gap-5">
+              <Label className="w-20 pt-3 font-semibold">Height:</Label>
+              <Input className="flex-1 min-w-0" defaultValue="25px" />
             </View>
           </PopoverContent>
         </Popover>
       </View>
-      <View style={{ height: height }} className='p-6 gap-24'>
+      <View style={{ height: height }} className="gap-24 p-6">
         <Popover>
-          <PopoverTrigger className='w-full' variant={'secondary'}>
+          <PopoverTrigger className="w-full" variant={"secondary"}>
             {({ pressed }) => (
               <Text
                 className={cn(
-                  pressed ? 'opacity-70' : '',
-                  buttonTextVariants({ variant: 'secondary' })
+                  pressed ? "opacity-70" : "",
+                  buttonTextVariants({ variant: "secondary" })
                 )}
               >
                 Auto-Y (right 300)
               </Text>
             )}
           </PopoverTrigger>
-          <PopoverContent className='gap-6' width={300} align='right'>
+          <PopoverContent className="gap-6" width={300} align="right">
             <View>
-              <Text className='text-2xl font-bold text-foreground'>
+              <Text className="text-2xl font-bold text-foreground">
                 Dimensions
               </Text>
-              <Text className='text-lg text-muted-foreground'>
+              <Text className="text-lg text-muted-foreground">
                 Set the dimensions for the layer.
               </Text>
             </View>
-            <View className='flex-row gap-5 items-center'>
-              <Label className='pt-3 font-semibold w-20'>Width:</Label>
-              <Input className='flex-1' defaultValue='100%' />
+            <View className="flex-row items-center gap-5">
+              <Label className="w-20 pt-3 font-semibold">Width:</Label>
+              <Input className="flex-1 min-w-0" defaultValue="100%" />
             </View>
-            <View className='flex-row gap-5 items-center'>
-              <Label className='pt-3 font-semibold w-20'>Height:</Label>
-              <Input className='flex-1' defaultValue='25px' />
+            <View className="flex-row items-center gap-5">
+              <Label className="w-20 pt-3 font-semibold">Height:</Label>
+              <Input className="flex-1 min-w-0" defaultValue="25px" />
             </View>
           </PopoverContent>
         </Popover>
         <Popover>
-          <PopoverTrigger className='w-full' variant={'secondary'}>
+          <PopoverTrigger className="w-full" variant={"secondary"}>
             {({ pressed }) => (
               <Text
                 className={cn(
-                  pressed ? 'opacity-70' : '',
-                  buttonTextVariants({ variant: 'secondary' })
+                  pressed ? "opacity-70" : "",
+                  buttonTextVariants({ variant: "secondary" })
                 )}
               >
                 Auto-Y (center 300)
               </Text>
             )}
           </PopoverTrigger>
-          <PopoverContent className='gap-6' width={300} align='center'>
+          <PopoverContent className="gap-6" width={300} align="center">
             <View>
-              <Text className='text-2xl font-bold text-foreground'>
+              <Text className="text-2xl font-bold text-foreground">
                 Dimensions
               </Text>
-              <Text className='text-lg text-muted-foreground'>
+              <Text className="text-lg text-muted-foreground">
                 Set the dimensions for the layer.
               </Text>
             </View>
-            <View className='flex-row gap-5 items-center'>
-              <Label className='pt-3 font-semibold w-20'>Width:</Label>
-              <Input className='flex-1' defaultValue='100%' />
+            <View className="flex-row items-center gap-5">
+              <Label className="w-20 pt-3 font-semibold">Width:</Label>
+              <Input className="flex-1 min-w-0" defaultValue="100%" />
             </View>
-            <View className='flex-row gap-5 items-center'>
-              <Label className='pt-3 font-semibold w-20'>Height:</Label>
-              <Input className='flex-1' defaultValue='25px' />
+            <View className="flex-row items-center gap-5">
+              <Label className="w-20 pt-3 font-semibold">Height:</Label>
+              <Input className="flex-1 min-w-0" defaultValue="25px" />
             </View>
           </PopoverContent>
         </Popover>
