878d82d5c2512809e74a84fdb6a1293cdf4a5df7
the-simian <harlinjesse@gmail.com>
2024-02-05 19:39:10 -0600

    change to zach's solution

--- components/ui/checkbox.tsx
+++ components/ui/checkbox.tsx
@@ -1,13 +1,8 @@
-import React from 'react';
 import { Check } from 'lucide-react-native';
-
+import * as React from 'react';
+import { Pressable } from 'react-native';
+import Animated, { FadeIn } from 'react-native-reanimated';
 import { cn } from '~/lib/utils';
-import { Pressable, View } from 'react-native';
-import Animated, {
-  Easing,
-  useSharedValue,
-  withTiming,
-} from 'react-native-reanimated';
 
 interface CheckboxProps {
   value: boolean;
@@ -16,47 +11,18 @@ interface CheckboxProps {
   iconSize?: number;
 }
 
-interface AnimatedCheckProps {
-  value: boolean;
-  size: number;
-  className: string;
-  opacity: number;
-}
-
-const AnimatedCheck = Animated.createAnimatedComponent(
-  React.forwardRef(
-    (
-      { value, size, className, opacity, ...props }: AnimatedCheckProps,
-      ref: React.ForwardedRef<any>
-    ) => {
-      return (
-        <View ref={ref} style={{ opacity }}>
-          <Check size={size} className={className} {...props} />
-        </View>
-      );
-    }
-  )
-);
-
 const Checkbox = React.forwardRef<
   React.ElementRef<typeof Pressable>,
   Omit<React.ComponentPropsWithoutRef<typeof Pressable>, 'onPress'> &
     CheckboxProps
 >(({ className, value, onChange, iconClass, iconSize = 16, ...props }, ref) => {
-  const opacity = useSharedValue(0);
-
-  opacity.value = withTiming(value === true ? 1.0 : 0.0, {
-    duration: 250,
-    easing: Easing.inOut(Easing.ease),
-  });
-
   return (
     <Pressable
       ref={ref}
       role='checkbox'
       accessibilityState={{ checked: value }}
       className={cn(
-        'peer h-7 w-7 shrink-0 flex items-center bg-card justify-center rounded-md border border-primary ring-offset-background disabled:cursor-not-allowed disabled:opacity-50',
+        'peer h-7 w-7 shrink-0 flex items-center bg-card justify-center rounded-md border border-primary web:ring-offset-background web:disabled:cursor-not-allowed web:disabled:opacity-50',
         className
       )}
       onPress={() => {
@@ -64,14 +30,11 @@ const Checkbox = React.forwardRef<
       }}
       {...props}
     >
-      <View />
-
-      <AnimatedCheck
-        size={iconSize || 16}
-        className={cn('text-foreground', iconClass) || ''}
-        value={value}
-        opacity={opacity}
-      />
+      {value && (
+        <Animated.View entering={FadeIn.duration(200)}>
+          <Check size={iconSize} className={cn('text-foreground', iconClass)} />
+        </Animated.View>
+      )}
     </Pressable>
   );
 });
