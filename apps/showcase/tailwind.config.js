f4d92ddf7fc5f2eab3ed17dd8be1849361261468
Zach Nugent <mrzachnugent@gmail.com>
2024-02-25 13:52:31 -0500

    fix missing native styling by watching @rnr in tailwind config

--- apps/showcase/tailwind.config.js
+++ apps/showcase/tailwind.config.js
@@ -3,7 +3,11 @@ const { hairlineWidth } = require('nativewind/theme');
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: 'class',
-  content: ['./app/**/*.{ts,tsx}', './components/**/*.{ts,tsx}'],
+  content: [
+    './app/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './node_modules/@rnr/**/*.{ts,tsx}',
+  ],
   presets: [require('nativewind/preset')],
   theme: {
     extend: {
