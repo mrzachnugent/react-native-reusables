6ac98765653ce34b5cba1f2e11025cc708004b5e
Zach Nugent <mrzachnugent@gmail.com>
2024-02-25 14:50:19 -0500

    add clean script for docs + remove radix deps in showcase

--- apps/showcase/app/+not-found.tsx
+++ apps/showcase/app/+not-found.tsx
@@ -1,17 +1,16 @@
 import { Link, Stack } from 'expo-router';
-import { Text, View } from 'react-native';
+import { View } from 'react-native';
+import { Text } from '~/components/ui/typography';
 
 export default function NotFoundScreen() {
   return (
     <>
       <Stack.Screen options={{ title: 'Oops!' }} />
       <View className='flex-1 justify-center gap-3 items-center'>
-        <Text className='text-foreground text-3xl'>
-          This screen doesn't exist.
-        </Text>
+        <Text className='text-3xl'>This screen doesn't exist.</Text>
         <View className='h-2' />
         <Link href='/'>
-          <Text className='text-foreground'>Go Home</Text>
+          <Text>Go Home</Text>
         </Link>
       </View>
     </>
