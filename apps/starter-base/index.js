0a6761e38db6da1331d52efc5d0b8e6157b7c023
Zach Nugent <mrzachnugent@gmail.com>
2024-03-02 14:14:35 -0500

    fix(starter-base): missing deps

--- apps/starter-base/index.js
+++ apps/starter-base/index.js
@@ -1,7 +1,6 @@
 import { registerRootComponent } from 'expo';
 import { ExpoRoot } from 'expo-router';
 
-// THIS FILE IS ONLY NEEDED IN A MONOREPO
 // https://docs.expo.dev/router/reference/troubleshooting/#expo_router_app_root-not-defined
 
 // Must be exported or Fast Refresh won't update the context
