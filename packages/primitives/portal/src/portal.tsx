7312bd89b4821ec2ba235156200bdd6484384a3b
Zach Nugent <mrzachnugent@gmail.com>
2024-04-28 12:41:13 -0400

    feat(primitives): add useModalPortalRoot hook to portal

--- packages/primitives/portal/src/portal.tsx
+++ packages/primitives/portal/src/portal.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { Platform, type View, type ViewStyle } from 'react-native';
 import { create } from 'zustand';
 
 const DEFAULT_PORTAL_HOST = 'INTERNAL_PRIMITIVE_DEFAULT_HOST_NAME';
@@ -7,17 +8,10 @@ type PortalMap = Map<string, React.ReactNode>;
 type PortalHostMap = Map<string, PortalMap>;
 
 const usePortal = create<{ map: PortalHostMap }>(() => ({
-  map: new Map<string, PortalMap>().set(
-    DEFAULT_PORTAL_HOST,
-    new Map<string, React.ReactNode>()
-  ),
+  map: new Map<string, PortalMap>().set(DEFAULT_PORTAL_HOST, new Map<string, React.ReactNode>()),
 }));
 
-const updatePortal = (
-  hostName: string,
-  name: string,
-  children: React.ReactNode
-) => {
+const updatePortal = (hostName: string, name: string, children: React.ReactNode) => {
   usePortal.setState((prev) => {
     const next = new Map(prev.map);
     const portal = next.get(hostName) ?? new Map<string, React.ReactNode>();
@@ -37,9 +31,7 @@ const removePortal = (hostName: string, name: string) => {
 };
 
 export function PortalHost({ name = DEFAULT_PORTAL_HOST }: { name?: string }) {
-  const portalMap =
-    usePortal((state) => state.map).get(name) ??
-    new Map<string, React.ReactNode>();
+  const portalMap = usePortal((state) => state.map).get(name) ?? new Map<string, React.ReactNode>();
   if (portalMap.size === 0) return null;
   return <>{Array.from(portalMap.values())}</>;
 }
@@ -64,4 +56,27 @@ export function Portal({
   }, [hostName, name]);
 
   return null;
+}
+
+const ROOT: ViewStyle = {
+  flex: 1,
+};
+
+export function useModalPortalRoot() {
+  const ref = React.useRef<View>(null);
+  const [sideOffset, setSideOffSet] = React.useState(0);
+
+  const onLayout = React.useCallback(() => {
+    if (Platform.OS === 'web') return;
+    ref.current?.measure((_x, _y, _width, _height, _pageX, pageY) => {
+      setSideOffSet(-pageY);
+    });
+  }, []);
+
+  return {
+    ref,
+    sideOffset,
+    onLayout,
+    style: ROOT,
+  };
 }
