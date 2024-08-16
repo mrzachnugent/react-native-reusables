5bce66b82d93cdf988b8f39657c2d5b5a84cc035
Masum Gökyüz <masumgokyuz@gmail.com>
2024-08-16 18:34:21 +0300

    fix(ui): switch component ui bug
    fix(docs): typo fix

--- packages/reusables/src/components/ui/switch.tsx
+++ packages/reusables/src/components/ui/switch.tsx
@@ -72,6 +72,7 @@ const SwitchNative = React.forwardRef<
       <SwitchPrimitives.Root
         className={cn(
           'flex-row h-8 w-[46px] shrink-0 items-center rounded-full border-2 border-transparent',
+          props.checked ? 'bg-primary' : 'bg-input',
           className
         )}
         {...props}
