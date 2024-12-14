8b3793abdac8b4303c5308afe73ab1462d40c378
Jesus Ayala <jayala08@gmail.com>
2024-12-14 14:18:34 -0300

    fix: fix select dropdown behind dialog

--- packages/reusables/src/components/ui/select.tsx
+++ packages/reusables/src/components/ui/select.tsx
@@ -76,7 +76,7 @@ const SelectContent = React.forwardRef<
   return (
     <SelectPrimitive.Portal hostName={portalHost}>
       <SelectPrimitive.Overlay style={Platform.OS !== 'web' ? StyleSheet.absoluteFill : undefined}>
-        <Animated.View entering={FadeIn} exiting={FadeOut}>
+        <Animated.View className='z-50' entering={FadeIn} exiting={FadeOut}>
           <SelectPrimitive.Content
             ref={ref}
             className={cn(
