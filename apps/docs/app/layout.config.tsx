e9fd7edcd0e11ca3f5764e23ed309fabe77e677d
Zach Nugent <mrzachnugent@gmail.com>
2025-01-10 10:59:31 -0500

    feat(docs): init new docs app

---
+++ apps/docs/app/layout.config.tsx
@@ -0,0 +1,22 @@
+import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
+
+/**
+ * Shared layout configurations
+ *
+ * you can configure layouts individually from:
+ * Home Layout: app/(home)/layout.tsx
+ * Docs Layout: app/docs/layout.tsx
+ */
+export const baseOptions: BaseLayoutProps = {
+  nav: {
+    // can be JSX too!
+    title: 'My App',
+  },
+  links: [
+    {
+      text: 'Documentation',
+      url: '/docs',
+      active: 'nested-url',
+    },
+  ],
+};
