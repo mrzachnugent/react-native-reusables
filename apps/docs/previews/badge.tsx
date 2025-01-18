26f7ab9fb9745650c57ed1de6ed4648b66000231
Zach Nugent <mrzachnugent@gmail.com>
2025-01-18 14:15:30 -0500

    feat(docs): add badge + use preview card in alert examples

---
+++ apps/docs/previews/badge.tsx
@@ -0,0 +1,10 @@
+import { Badge } from '~/components/ui/badge';
+import { Text } from '~/components/ui/text';
+
+export function BadgePreview() {
+  return (
+    <Badge>
+      <Text>Badge</Text>
+    </Badge>
+  );
+}
