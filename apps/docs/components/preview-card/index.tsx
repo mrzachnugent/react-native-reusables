976babba3d793cc92ec4abc830513a705ab172a9
Zach Nugent <mrzachnugent@gmail.com>
2025-01-20 15:21:07 -0500

    feat(docs): add card

--- apps/docs/components/preview-card/index.tsx
+++ apps/docs/components/preview-card/index.tsx
@@ -1,9 +1,13 @@
 import { cookies } from 'next/headers';
-import { type PreviewCardClientProps, type Platform, PreviewCardClient } from './client';
+import {
+  type PreviewCardClientProps,
+  type Platform,
+  PreviewCardClient,
+} from './preview-card-client';
 
 type PreviewCardProps = Omit<PreviewCardClientProps, 'platformCookie'>;
 
-export async function PreviewCard(props: PreviewCardProps) {
+export function PreviewCard(props: PreviewCardProps) {
   const cookieStore = cookies();
   const platformCookie = cookieStore.get('platform')?.value as Platform;
   return <PreviewCardClient platformCookie={platformCookie} {...props} />;
