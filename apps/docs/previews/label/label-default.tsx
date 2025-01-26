139edcdb7217d05b38631e14809d7d0a025b032b
Zach Nugent <mrzachnugent@gmail.com>
2025-01-26 15:31:40 -0500

    feat(docs): add label

---
+++ apps/docs/previews/label/label-default.tsx
@@ -0,0 +1,15 @@
+import { View } from '@/components/react-native';
+import { Checkbox } from '~/components/ui/checkbox';
+import { Label } from '~/components/ui/label';
+
+export function LabelPreview() {
+  return (
+    <View className='flex-row gap-3 items-center'>
+      {/* TODO */}
+      <Checkbox aria-labelledby='terms' />
+      <Label nativeID='terms' htmlFor='terms'>
+        Accept terms and conditions
+      </Label>
+    </View>
+  );
+}
