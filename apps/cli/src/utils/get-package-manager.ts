a1a9d9b2cb9c1bc1c443f4b6a79e169ba58369b4
Zach Nugent <mrzachnugent@gmail.com>
2025-04-18 14:56:17 -0400

    fix(cli): detect bun as pck manager

--- apps/cli/src/utils/get-package-manager.ts
+++ apps/cli/src/utils/get-package-manager.ts
@@ -2,12 +2,15 @@ import { detect } from '@antfu/ni';
 
 export async function getPackageManager(
   targetDir: string
-): Promise<'yarn' | 'pnpm' | 'bun' | 'npm'> {
+): Promise<'yarn' | 'pnpm' | 'bun' | 'npm' | 'deno'> {
   const packageManager = await detect({ programmatic: true, cwd: targetDir });
 
-  if (packageManager === 'yarn@berry') return 'yarn';
-  if (packageManager === 'pnpm@6') return 'pnpm';
-  if (packageManager === 'bun') return 'bun';
+  if (packageManager === 'yarn@berry') {
+    return 'yarn';
+  }
+  if (packageManager === 'pnpm@6') {
+    return 'pnpm';
+  }
 
   return packageManager ?? 'npm';
 }
