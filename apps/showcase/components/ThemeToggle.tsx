2c31a79e1921709ad32c4981b116a771bb6d9f9e
Zach Nugent <mrzachnugent@gmail.com>
2025-05-21 15:50:25 -0400

    fix: theme toggle

--- apps/showcase/components/ThemeToggle.tsx
+++ apps/showcase/components/ThemeToggle.tsx
@@ -16,9 +16,9 @@ export function ThemeToggle() {
   return (
     <Pressable
       onPress={toggleColorScheme}
-      className='web:ring-offset-background web:transition-colors web:focus-visible:outline-none web:focus-visible:ring-2 web:focus-visible:ring-ring web:focus-visible:ring-offset-2'
+      className='web:ring-offset-background web:transition-colors web:focus-visible:outline-none web:focus-visible:ring-2 web:focus-visible:ring-ring web:focus-visible:ring-offset-2 active:opacity-70'
     >
-      <View className='flex-1 aspect-square pt-0.5 justify-center items-start web:px-5 active:opacity-70'>
+      <View className='flex-1 aspect-square pt-0.5 justify-center items-start web:px-5'>
         {isDarkColorScheme ? (
           <MoonStar className='text-foreground' size={23} strokeWidth={1.25} />
         ) : (
