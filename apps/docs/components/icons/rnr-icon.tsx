478019853bbefeb0a3ac3764f012f8a2152ddd19
Zach Nugent <mrzachnugent@gmail.com>
2025-06-08 15:18:18 -0400

    feat(docs): minor adjustments to landing page

--- apps/docs/components/icons/rnr-icon.tsx
+++ apps/docs/components/icons/rnr-icon.tsx
@@ -2,59 +2,24 @@ import { cn } from '@docs/lib/utils';
 
 export function RnrIcon({
   className,
-  strokeWidth = 24,
   reactPathClassName,
   ...props
 }: React.ComponentProps<'svg'> & { reactPathClassName?: string }) {
   return (
     <svg
-      width='882'
-      height='803'
-      viewBox='0 0 882 803'
+      viewBox='0 0 22 20'
       fill='none'
       xmlns='http://www.w3.org/2000/svg'
-      className={cn('h-6 w-6', className)}
+      className={cn('h-6', className)}
+      shapeRendering='crispEdges'
       {...props}
     >
       <path
-        d='M441 591.74C672.958 591.74 860.997 507.15 860.997 402.799C860.997 298.448 672.958 213.858 441 213.858C209.042 213.858 20.9948 298.448 20.9948 402.799C20.9948 507.15 209.042 591.74 441 591.74Z'
+        d='M12.9049 9.9809L11.0001 11.8969M12.5239 7.87329L8.90486 11.5137M11.0001 14.5252C16.5229 14.5252 21 12.4993 21 10C21 7.50083 16.5229 5.47487 11.0001 5.47487C5.4773 5.47487 1 7.50083 1 10C1 12.4993 5.4773 14.5252 11.0001 14.5252ZM7.10292 12.2626C9.86441 17.0723 13.8476 19.9582 16 18.7087C18.1524 17.459 17.6586 12.547 14.8971 7.73746C12.1358 2.92758 8.15235 0.041657 6.00024 1.29136C3.84774 2.54087 4.34162 7.45294 7.10292 12.2626ZM7.10292 7.73746C4.34162 12.5471 3.84793 17.459 6.00005 18.7087C8.15254 19.9582 12.1358 17.0721 14.8971 12.2626C17.6586 7.45275 18.1524 2.54106 16.0001 1.29136C13.8476 0.0416569 9.86441 2.92777 7.10292 7.73746Z'
         stroke='currentColor'
-        strokeWidth={strokeWidth}
-        className={cn('opacity-70 dark:opacity-50', reactPathClassName)}
+        strokeLinecap='round'
+        className={reactPathClassName}
       />
-      <path
-        d='M277.318 497.27C393.301 698.09 560.598 818.588 650.995 766.416C741.4 714.237 720.657 509.141 604.674 308.328C488.699 107.5 321.395 -12.9976 231.005 39.1817C140.6 91.3531 161.343 296.449 277.318 497.27Z'
-        stroke='currentColor'
-        strokeWidth={strokeWidth}
-        className={cn('opacity-70 dark:opacity-50', reactPathClassName)}
-      />
-      <path
-        d='M277.318 308.328C161.343 509.149 140.608 714.237 230.997 766.416C321.402 818.588 488.699 698.083 604.674 497.27C720.657 296.441 741.4 91.3609 651.003 39.1817C560.598 -12.9976 393.301 107.508 277.318 308.328Z'
-        stroke='currentColor'
-        strokeWidth={strokeWidth}
-        className={cn('opacity-70 dark:opacity-50', reactPathClassName)}
-      />
-      <g clipPath='url(#clip0_5_26)'>
-        <path
-          d='M521 402L441 482'
-          stroke='currentColor'
-          strokeWidth={Number(strokeWidth) * 1.333333333333}
-          strokeLinecap='round'
-          strokeLinejoin='round'
-        />
-        <path
-          d='M505 314L353 466'
-          stroke='currentColor'
-          strokeWidth={Number(strokeWidth) * 1.333333333333}
-          strokeLinecap='round'
-          strokeLinejoin='round'
-        />
-      </g>
-      <defs>
-        <clipPath id='clip0_5_26'>
-          <rect width='256' height='256' fill='white' transform='translate(313 274)' />
-        </clipPath>
-      </defs>
     </svg>
   );
 }
