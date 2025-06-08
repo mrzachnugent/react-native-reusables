fb92f8df20b6b4202a169acd485d2ad7c9406154
Zach Nugent <mrzachnugent@gmail.com>
2025-06-08 14:27:20 -0400

    feat: change inline rem to 16

--- apps/showcase/metro.config.js
+++ apps/showcase/metro.config.js
@@ -23,4 +23,4 @@ config.resolver = {
   sourceExts: [...resolver.sourceExts, 'mjs', 'cjs'],
 };
 
-module.exports = withNativeWind(config, { input: './global.css' });
+module.exports = withNativeWind(config, { input: './global.css', inlineRem: 16 });
--- packages/templates/starter-base/metro.config.js
+++ packages/templates/starter-base/metro.config.js
@@ -3,4 +3,4 @@ const { withNativeWind } = require('nativewind/metro');
 
 const config = getDefaultConfig(__dirname);
 
-module.exports = withNativeWind(config, { input: './global.css' });
+module.exports = withNativeWind(config, { input: './global.css', inlineRem: 16 });
