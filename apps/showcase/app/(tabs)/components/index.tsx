5db21a46672d963a090fdaf7773cafd674eccb84
Zach Nugent <mrzachnugent@gmail.com>
2025-06-23 13:29:49 -0400

    fix: icon component interop for size class + update component and primitives list

--- apps/showcase/app/(tabs)/components/index.tsx
+++ apps/showcase/app/(tabs)/components/index.tsx
@@ -1,14 +1,15 @@
-import { useScrollToTop } from '@react-navigation/native';
-import { FlashList } from '@shopify/flash-list';
-import { Link } from 'expo-router';
-import * as React from 'react';
-import { View } from 'react-native';
 import { Button } from '@/components/ui/button';
+import { Icon } from '@/components/ui/icon';
 import { Input } from '@/components/ui/input';
 import { Text } from '@/components/ui/text';
-import { COMPONENTS } from '@showcase/lib/constants';
-import { ChevronRight } from '@/lib/icons/ChevronRight';
 import { cn } from '@/lib/utils';
+import { useScrollToTop } from '@react-navigation/native';
+import { FlashList } from '@shopify/flash-list';
+import { COMPONENTS } from '@showcase/lib/constants';
+import { Link } from 'expo-router';
+import { ChevronRight } from 'lucide-react-native';
+import * as React from 'react';
+import { View } from 'react-native';
 
 export default function ComponentsScreen() {
   const [search, setSearch] = React.useState('');
@@ -20,33 +21,35 @@ export default function ComponentsScreen() {
     : COMPONENTS.filter((item) => item.toLowerCase().includes(search.toLowerCase()));
   return (
     <View className='flex-1 px-4'>
-      <View className='py-4'>
-        <Input
-          placeholder='Search UI...'
-          clearButtonMode='always'
-          value={search}
-          onChangeText={setSearch}
-        />
-      </View>
       <FlashList
         ref={ref}
         data={data}
         className='native:overflow-hidden rounded-t-lg'
         estimatedItemSize={49}
         showsVerticalScrollIndicator={false}
+        ListHeaderComponent={
+          <View className='py-4'>
+            <Input
+              placeholder='Search UI...'
+              clearButtonMode='always'
+              value={search}
+              onChangeText={setSearch}
+            />
+          </View>
+        }
         renderItem={({ item, index }) => (
           <Link href={`/${item}`} asChild>
             <Button
               variant='secondary'
               size='lg'
               className={cn(
-                'bg-secondary/40 pl-4 pr-1.5 border-x border-t border-foreground/5 rounded-none flex-row justify-between',
+                'bg-secondary/40 pl-4 pr-2.5 border-x border-t border-foreground/5 rounded-none flex-row justify-between h-12',
                 index === 0 && 'rounded-t-lg',
                 index === data.length - 1 && 'border-b rounded-b-lg'
               )}
             >
-              <Text className='text-xl'>{toOptions(item)}</Text>
-              <ChevronRight className='text-foreground/50' />
+              <Text className='text-base'>{toOptions(item)}</Text>
+              <Icon as={ChevronRight} className='text-foreground/50 size-4' />
             </Button>
           </Link>
         )}
