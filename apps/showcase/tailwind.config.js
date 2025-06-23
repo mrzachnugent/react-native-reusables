0f01cd523593cdba6f2a4e0b461184aa1babc153
Zach Nugent <mrzachnugent@gmail.com>
2025-06-23 15:52:46 -0400

    feat: improve avatar example

--- apps/showcase/tailwind.config.js
+++ apps/showcase/tailwind.config.js
@@ -46,6 +46,11 @@ module.exports = {
           foreground: 'hsl(var(--card-foreground))',
         },
       },
+      borderRadius: {
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)',
+      },
       borderWidth: {
         hairline: hairlineWidth(),
       },
--- packages/reusables/src/examples/avatar.tsx
+++ packages/reusables/src/examples/avatar.tsx
@@ -1,15 +1,57 @@
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { Text } from '@/components/ui/text';
-
-const SOURCE = { uri: 'https://github.com/mrzachnugent.png' };
+import { View } from 'react-native';
 
 export function AvatarPreview() {
   return (
-    <Avatar alt="Zach Nugent's Avatar">
-      <AvatarImage source={SOURCE} />
-      <AvatarFallback>
-        <Text>ZN</Text>
-      </AvatarFallback>
-    </Avatar>
+    <View className='flex-row gap-12 flex-wrap'>
+      <Avatar
+        alt='@mrzachnugent'
+        className='border-2 border-background web:border-0 web:ring-2 web:ring-background'
+      >
+        <AvatarImage source={{ uri: 'https://github.com/mrzachnugent.png' }} />
+        <AvatarFallback>
+          <Text>ZN</Text>
+        </AvatarFallback>
+      </Avatar>
+      <Avatar
+        alt='@shadcn'
+        className='border-2 border-background web:border-0 web:ring-2 web:ring-background rounded-lg'
+      >
+        <AvatarImage source={{ uri: 'https://github.com/shadcn.png' }} />
+        <AvatarFallback>
+          <Text>CN</Text>
+        </AvatarFallback>
+      </Avatar>
+      <View className='flex-row'>
+        <Avatar
+          alt='@mrzachnugent'
+          className='-mr-2 border-2 border-background web:border-0 web:ring-2 web:ring-background'
+        >
+          <AvatarImage source={{ uri: 'https://github.com/mrzachnugent.png' }} />
+          <AvatarFallback>
+            <Text>ZN</Text>
+          </AvatarFallback>
+        </Avatar>
+        <Avatar
+          alt='@leerob'
+          className='-mr-2 border-2 border-background web:border-0 web:ring-2 web:ring-background'
+        >
+          <AvatarImage source={{ uri: 'https://github.com/leerob.png' }} />
+          <AvatarFallback>
+            <Text>LR</Text>
+          </AvatarFallback>
+        </Avatar>
+        <Avatar
+          alt='@evilrabbit'
+          className='-mr-2 border-2 border-background web:border-0 web:ring-2 web:ring-background'
+        >
+          <AvatarImage source={{ uri: 'https://github.com/evilrabbit.png' }} />
+          <AvatarFallback>
+            <Text>ER</Text>
+          </AvatarFallback>
+        </Avatar>
+      </View>
+    </View>
   );
 }
