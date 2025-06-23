81cd525f884772e0c122305b0ad1f783b48384dc
Zach Nugent <mrzachnugent@gmail.com>
2025-06-23 13:44:25 -0400

    feat: update avatar

--- apps/showcase/app/avatar.tsx
+++ apps/showcase/app/avatar.tsx
@@ -1,18 +1,10 @@
 import { View } from 'react-native';
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
-import { Text } from '@/components/ui/text';
-
-const GITHUB_AVATAR_URI = 'https://github.com/mrzachnugent.png';
+import { AvatarPreview } from '@/examples/avatar';
 
 export default function AvatarScreen() {
   return (
     <View className='flex-1 justify-center items-center p-6 gap-12'>
-      <Avatar alt="Zach Nugent's Avatar">
-        <AvatarImage source={{ uri: GITHUB_AVATAR_URI }} />
-        <AvatarFallback>
-          <Text>ZN</Text>
-        </AvatarFallback>
-      </Avatar>
+      <AvatarPreview />
     </View>
   );
 }
