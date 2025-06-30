4a1c46c225e083483e6b999bc20b666b05cc6169
Zach Nugent <mrzachnugent@gmail.com>
2025-06-30 13:58:58 -0400

    feat: update collapsible

--- packages/reusables/src/examples/collapsible.tsx
+++ packages/reusables/src/examples/collapsible.tsx
@@ -2,53 +2,33 @@ import { Button } from '@/components/ui/button';
 import { Collapsible, CollapsibleContent, CollapsibleTrigger } from '@/components/ui/collapsible';
 import { Icon } from '@/components/ui/icon';
 import { ChevronsUpDown } from 'lucide-react-native';
-import { Platform, Text, View } from 'react-native';
-import Animated, { FadeInDown, LinearTransition } from 'react-native-reanimated';
+import { Text, View } from 'react-native';
 
 export function CollapsiblePreview() {
   return (
-    <Collapsible asChild>
-      <Animated.View layout={Platform.OS !== 'web' ? LinearTransition : undefined}>
-        <View className='w-full max-w-[350px] gap-2'>
-          <View className='flex flex-row items-center justify-between space-x-4 px-4'>
-            <Text className='text-foreground text-sm native:text-lg font-semibold'>
-              @peduarte starred 3 repositories
-            </Text>
-            <CollapsibleTrigger asChild>
-              <Button variant='ghost' size='icon'>
-                <Icon as={ChevronsUpDown} size={16} className='text-foreground' />
-                <Text className='sr-only'>Toggle</Text>
-              </Button>
-            </CollapsibleTrigger>
-          </View>
-          <View className='rounded-md border border-border px-4 py-3 '>
-            <Text className='text-foreground text-sm native:text-lg'>@radix-ui/primitives</Text>
-          </View>
-          <CollapsibleContent className='gap-2'>
-            <CollapsibleItem delay={100}>@radix-ui/react</CollapsibleItem>
-            <CollapsibleItem delay={200}>@stitches/core</CollapsibleItem>
-          </CollapsibleContent>
-        </View>
-      </Animated.View>
-    </Collapsible>
-  );
-}
-
-function CollapsibleItem({ children, delay }: { children: string; delay: number }) {
-  if (Platform.OS === 'web') {
-    return (
-      <View className='rounded-md border border-border px-4 py-3'>
-        <Text className='text-foreground text-sm'>{children}</Text>
+    <Collapsible className='flex flex-row w-[350px] flex-col gap-2'>
+      <View className='flex flex-row items-center justify-between gap-4 px-4'>
+        <Text className='text-foreground text-sm font-semibold'>
+          @peduarte starred 3 repositories
+        </Text>
+        <CollapsibleTrigger asChild>
+          <Button variant='ghost' size='icon' className='size-8'>
+            <Icon as={ChevronsUpDown} className='text-foreground' />
+            <Text className='sr-only'>Toggle</Text>
+          </Button>
+        </CollapsibleTrigger>
       </View>
-    );
-  }
-
-  return (
-    <Animated.View
-      entering={FadeInDown.duration(200).delay(delay)}
-      className='rounded-md border border-border px-4 py-3'
-    >
-      <Text className='text-foreground text-lg'>{children}</Text>
-    </Animated.View>
+      <View className='rounded-md border border-border px-4 py-2'>
+        <Text className='text-foreground text-sm"'>@radix-ui/primitives</Text>
+      </View>
+      <CollapsibleContent className='gap-2'>
+        <View className='rounded-md border border-border px-4 py-2'>
+          <Text className='text-foreground text-sm'>@radix-ui/react</Text>
+        </View>
+        <View className='rounded-md border border-border px-4 py-2'>
+          <Text className='text-foreground text-sm'>@stitches/core</Text>
+        </View>
+      </CollapsibleContent>
+    </Collapsible>
   );
 }
