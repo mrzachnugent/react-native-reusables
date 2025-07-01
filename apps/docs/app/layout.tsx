6132eb852a1084d01596093ad7c1f2353d72200a
Zach Nugent <mrzachnugent@gmail.com>
2025-07-01 13:53:02 -0400

    feat(docs): use the geist font

--- apps/docs/app/layout.tsx
+++ apps/docs/app/layout.tsx
@@ -1,16 +1,24 @@
 import { SafeAreaProvider } from '@docs/components/reusables';
-import './global.css';
+import { cn } from '@docs/lib/utils';
 import { RootProvider } from 'fumadocs-ui/provider';
-import { Inter } from 'next/font/google';
+import { Geist, Geist_Mono } from 'next/font/google';
 import type { ReactNode } from 'react';
+import './global.css';
 
-const inter = Inter({
+const fontSans = Geist({
   subsets: ['latin'],
+  variable: '--font-sans',
+});
+
+const fontMono = Geist_Mono({
+  subsets: ['latin'],
+  variable: '--font-mono',
+  weight: ['400'],
 });
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <html lang='en' className={inter.className} suppressHydrationWarning>
+    <html lang='en' className={cn(fontSans.variable, fontMono.variable)} suppressHydrationWarning>
       <body className='flex flex-col min-h-screen'>
         <SafeAreaProvider>
           <RootProvider>{children}</RootProvider>
