252e564ba2b8958205aaf7b3cdf740e7219e17c6
Zach Nugent <mrzachnugent@gmail.com>
2025-07-01 13:43:37 -0400

    feat: add hitslop for checkbox + improve native label example

--- packages/reusables/src/examples/label.tsx
+++ packages/reusables/src/examples/label.tsx
@@ -1,14 +1,18 @@
 import { Checkbox } from '@/components/ui/checkbox';
 import { Label } from '@/components/ui/label';
 import * as React from 'react';
-import { View } from 'react-native';
+import { Platform, View } from 'react-native';
 
 export function LabelPreview() {
   const [checked, setChecked] = React.useState(false);
   return (
-    <View className='flex-row gap-3 items-center'>
-      <Checkbox aria-labelledby='terms' checked={checked} onCheckedChange={setChecked} />
-      <Label nativeID='terms' htmlFor='terms'>
+    <View className='flex-row gap-2 items-center'>
+      <Checkbox aria-labelledby='terms' id='terms' checked={checked} onCheckedChange={setChecked} />
+      <Label
+        nativeID='terms'
+        htmlFor='terms'
+        onPress={Platform.select({ native: () => setChecked((prev) => !prev) })}
+      >
         Accept terms and conditions
       </Label>
     </View>
