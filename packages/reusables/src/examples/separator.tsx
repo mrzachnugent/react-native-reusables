fae284e44ea73bfb2389d335b0f6e9f2f2b8c8f8
Zach Nugent <mrzachnugent@gmail.com>
2025-07-02 15:48:39 -0400

    feat: update separator

---
+++ packages/reusables/src/examples/separator.tsx
@@ -0,0 +1,22 @@
+import { Separator } from '@/components/ui/separator';
+import { Text } from '@/components/ui/text';
+import { View } from 'react-native';
+
+export function SeparatorPreview() {
+  return (
+    <View>
+      <View className='gap-1'>
+        <Text className='text-sm leading-none font-medium'>Radix Primitives</Text>
+        <Text className='text-sm text-muted-foreground'>An open-source UI component library.</Text>
+      </View>
+      <Separator className='my-4' />
+      <View className='flex flex-row h-5 items-center gap-4 '>
+        <Text className='text-sm'>Blog</Text>
+        <Separator orientation='vertical' />
+        <Text className='text-sm'>Docs</Text>
+        <Separator orientation='vertical' />
+        <Text className='text-sm'>Source</Text>
+      </View>
+    </View>
+  );
+}
