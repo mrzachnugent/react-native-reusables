4cf10500edd3cb50e233f00a3c0d9afb9c577bf2
Zach Nugent <mrzachnugent@gmail.com>
2025-07-02 20:58:17 -0400

    feat: update textarea + fix input

--- packages/reusables/src/components/ui/input.tsx
+++ packages/reusables/src/components/ui/input.tsx
@@ -2,6 +2,8 @@ import { cn } from '@/lib/utils';
 import * as React from 'react';
 import { Platform, TextInput, type TextInputProps } from 'react-native';
 
+const DEFAULT_HIT_SLOP = 12;
+
 function Input({
   className,
   placeholderClassName,
@@ -12,7 +14,7 @@ function Input({
   return (
     <TextInput
       className={cn(
-        'dark:bg-input/30 border-input flex flex-row items-center h-9 w-full min-w-0 rounded-md border bg-background px-3 py-1 text-foreground shadow-sm',
+        'dark:bg-input/30 border-input flex flex-row items-center h-9 text-base leading-5 w-full min-w-0 rounded-md border bg-background px-3 py-1 text-foreground shadow-sm',
         props.editable === false &&
           cn(
             'opacity-50',
@@ -28,6 +30,7 @@ function Input({
         }),
         className
       )}
+      hitSlop={DEFAULT_HIT_SLOP}
       {...props}
     />
   );
