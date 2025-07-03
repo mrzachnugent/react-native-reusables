b42dfa32816b08c1cbe4491e3e365dac5b56ed32
Zach Nugent <mrzachnugent@gmail.com>
2025-07-03 09:50:02 -0400

    feat: add typography variants to text component

--- apps/showcase/app/typography.tsx
+++ apps/showcase/app/typography.tsx
@@ -1,67 +1,70 @@
 import { ScrollView, View } from 'react-native';
-import { BlockQuote, H1, H2, H3, Large, Lead, P } from '@/components/ui/typography';
+import { Text } from '@/components/ui/text';
 
 export default function TypographyScreen() {
   return (
-    <ScrollView contentContainerClassName='p-6 items-center' showsVerticalScrollIndicator={false}>
+    <ScrollView contentContainerClassName='p-6'>
       <View className='max-w-lg'>
-        <H1>The Rainbow Forest Adventure</H1>
+        <Text variant='h1'>The Rainbow Forest Adventure</Text>
         <View className='p-1.5' />
-        <P>
+        <Text variant='p'>
           Once upon a time, in a magical forest, there lived a curious rabbit named Whiskers.
           Whiskers loved exploring and discovering new things every day.
-        </P>
+        </Text>
         <View className='p-3' />
-        <H2>Whiskers' Discovery</H2>
-        <P>
+        <Text variant='h2'>Whiskers' Discovery</Text>
+        <Text variant='p'>
           One day, while hopping through the forest, Whiskers stumbled upon{' '}
-          <P className='font-medium'>a mysterious rainbow-colored flower</P>. The flower had the
-          power to make the forest come alive with vibrant colors and happy creatures.
-        </P>
-        <BlockQuote>
+          <Text variant='p' className='font-medium'>
+            a mysterious rainbow-colored flower
+          </Text>
+          . The flower had the power to make the forest come alive with vibrant colors and happy
+          creatures.
+        </Text>
+        <Text variant='blockquote'>
           "Oh, what a wonderful discovery!" exclaimed Whiskers. "I must share this magic with all my
           forest friends!"
-        </BlockQuote>
+        </Text>
         <View className='p-4' />
-        <H3>The Colorful Transformation</H3>
+        <Text variant='h3'>The Colorful Transformation</Text>
         <View className='p-0.5' />
-        <P>
+        <Text variant='p'>
           Whiskers excitedly gathered all the animals in the forest and showed them the magical
           rainbow flower. The animals were amazed and decided to plant more of these flowers to make
           their home even more magical.
-        </P>
+        </Text>
         <View className='p-1.5' />
-        <P>
+        <Text variant='p'>
           As the rainbow flowers bloomed, the entire forest transformed into a kaleidoscope of
           colors. Birds chirped in harmony, butterflies danced in the air, and even the trees swayed
           to the rhythm of the wind.
-        </P>
+        </Text>
         <View className='p-3' />
-        <H3>The Enchanted Celebration</H3>
+        <Text variant='h3'>The Enchanted Celebration</Text>
         <View className='p-0.5' />
-        <P>
+        <Text variant='p'>
           The animals decided to celebrate their enchanted forest with a grand feast. They gathered
           nuts, berries, and fruits from the colorful trees and shared stories of their adventures.
           The joyous laughter echoed through the Rainbow Forest.
-        </P>
+        </Text>
         <View className='p-1.5' />
-        <Lead>
+        <Text variant='lead'>
           And so, the Rainbow Forest became a place of wonder and happiness, where Whiskers and all
           the animals lived together in harmony.
-        </Lead>
+        </Text>
         <View className='p-3' />
-        <H3>The Never-ending Magic</H3>
+        <Text variant='h3'>The Never-ending Magic</Text>
         <View className='p-0.5' />
-        <P>
+        <Text variant='p'>
           The magic of the rainbow flowers continued to spread, reaching other parts of the world.
           Soon, forests everywhere became vibrant and alive, thanks to the discovery of Whiskers and
           the enchanted Rainbow Forest.
-        </P>
+        </Text>
         <View className='p-1.5' />
-        <Large>
+        <Text variant='large'>
           The moral of the story is: embrace the magic of discovery, share joy with others, and
           watch as the world transforms into a colorful and beautiful place.
-        </Large>
+        </Text>
         <View className='p-3' />
       </View>
     </ScrollView>
--- packages/reusables/src/components/ui/text.tsx
+++ packages/reusables/src/components/ui/text.tsx
@@ -1,26 +1,63 @@
 import { cn } from '@/lib/utils';
 import * as Slot from '@rn-primitives/slot';
+import { cva, VariantProps } from 'class-variance-authority';
 import * as React from 'react';
-import { Text as RNText } from 'react-native';
+import { Platform, Text as RNText } from 'react-native';
+
+const textVariants = cva(
+  cn(
+    'text-base text-foreground',
+    Platform.select({
+      web: 'select-text',
+    })
+  ),
+  {
+    variants: {
+      variant: {
+        default: '',
+        h1: cn(
+          'text-center text-4xl font-extrabold tracking-tight',
+          Platform.select({ web: 'scroll-m-20 text-balance' })
+        ),
+        h2: cn(
+          'border-b border-border pb-2 text-3xl font-semibold tracking-tight',
+          Platform.select({ web: 'scroll-m-20 first:mt-0' })
+        ),
+        h3: cn('text-2xl font-semibold tracking-tight', Platform.select({ web: 'scroll-m-20' })),
+        h4: cn('text-xl font-semibold tracking-tight', Platform.select({ web: 'scroll-m-20' })),
+        p: 'leading-7 mt-6',
+        blockquote: 'mt-4 sm:mt-6 border-l-2 pl-3 sm:pl-6 italic',
+        code: cn(
+          'bg-muted relative rounded px-[0.3rem] py-[0.2rem] text-sm font-semibold',
+          Platform.select({ android: 'font-sans', default: 'font-mono' })
+        ),
+        lead: 'text-muted-foreground text-xl',
+        large: 'text-lg font-semibold',
+        small: 'text-sm leading-none font-medium',
+        muted: 'text-muted-foreground text-sm',
+      },
+    },
+    defaultVariants: {
+      variant: 'default',
+    },
+  }
+);
 
 const TextClassContext = React.createContext<string | undefined>(undefined);
 
 function Text({
   className,
   asChild = false,
+  variant = 'default',
   ...props
-}: React.ComponentProps<typeof RNText> & {
-  ref?: React.RefObject<RNText>;
-  asChild?: boolean;
-}) {
+}: React.ComponentProps<typeof RNText> &
+  VariantProps<typeof textVariants> & {
+    ref?: React.RefObject<RNText>;
+    asChild?: boolean;
+  }) {
   const textClass = React.useContext(TextClassContext);
   const Component = asChild ? Slot.Text : RNText;
-  return (
-    <Component
-      className={cn('text-base text-foreground web:select-text', textClass, className)}
-      {...props}
-    />
-  );
+  return <Component className={cn(textVariants({ variant }), textClass, className)} {...props} />;
 }
 
 export { Text, TextClassContext };
