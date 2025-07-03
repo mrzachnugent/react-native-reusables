22a9581d2364f6f0bc43416653d87357f901d2ef
Zach Nugent <mrzachnugent@gmail.com>
2025-07-02 21:32:22 -0400

    feat: add toggle and haptics to next config

--- apps/showcase/app/toggle.tsx
+++ apps/showcase/app/toggle.tsx
@@ -1,20 +1,10 @@
-import * as React from 'react';
+import { TogglePreview } from '@/examples';
 import { View } from 'react-native';
-import { Toggle, ToggleIcon } from '@/components/ui/toggle';
-import { Bold } from '@/lib/icons/Bold';
 
-export default function ToggleUniversalcreen() {
-  const [pressed, setPressed] = React.useState(false);
+export default function ToggleScreen() {
   return (
     <View className='flex-1 justify-center items-center p-6 gap-12'>
-      <Toggle
-        pressed={pressed}
-        onPressedChange={setPressed}
-        aria-label='Toggle bold'
-        variant='outline'
-      >
-        <ToggleIcon icon={Bold} size={18} />
-      </Toggle>
+      <TogglePreview />
     </View>
   );
 }
