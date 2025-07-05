2a66304b8d3b633f7b666bf984f9c4e7b16ec996
Zach Nugent <mrzachnugent@gmail.com>
2025-07-05 15:00:40 -0400

    feat(showcase): add geist font + settings screen + icons for component list

--- apps/showcase/app/index.tsx
+++ apps/showcase/app/index.tsx
@@ -6,11 +6,13 @@ import { cn } from '@/registry/new-york/lib/utils';
 import { useScrollToTop } from '@react-navigation/native';
 import { FlashList } from '@shopify/flash-list';
 import { COMPONENTS } from '@showcase/lib/constants';
-import { Link } from 'expo-router';
+import { Link, useNavigation } from 'expo-router';
 import { ChevronRight } from 'lucide-react-native';
 import { cssInterop } from 'nativewind';
 import * as React from 'react';
 import { View } from 'react-native';
+import Animated from 'react-native-reanimated';
+import { useReanimatedHeaderHeight } from 'react-native-screens/reanimated';
 
 cssInterop(FlashList, { className: 'style', contentContainerClassName: 'contentContainerStyle' });
 
@@ -18,20 +20,23 @@ export default function ComponentsScreen() {
   const [search, setSearch] = React.useState('');
   const ref = React.useRef(null);
   useScrollToTop(ref);
+  const headerHeight = useReanimatedHeaderHeight();
 
   const data = !search
     ? COMPONENTS
-    : COMPONENTS.filter((item) => item.toLowerCase().includes(search.toLowerCase()));
+    : COMPONENTS.filter((item) => item.name.toLowerCase().includes(search.toLowerCase()));
 
   return (
     <FlashList
       ref={ref}
       data={data}
       contentInsetAdjustmentBehavior='automatic'
-      className='rounded-t-lg'
       contentContainerClassName='px-4'
       estimatedItemSize={49}
+      keyboardShouldPersistTaps='handled'
+      keyboardDismissMode='interactive'
       showsVerticalScrollIndicator={false}
+      stickyHeaderIndices={[0]}
       ListHeaderComponent={
         <View className='pb-4'>
           <Input
@@ -43,19 +48,22 @@ export default function ComponentsScreen() {
         </View>
       }
       renderItem={({ item, index }) => (
-        <Link href={`/${item}`} asChild>
+        <Link href={`/${item.name}`} asChild>
           <Button
             variant='outline'
             size='lg'
             unstable_pressDelay={100}
             className={cn(
-              'pl-4 pr-2.5  border-b-0 border-border rounded-none flex-row justify-between',
+              'pl-4 pr-3.5 dark:bg-background border-b-0 border-border rounded-none flex-row justify-between',
               index === 0 && 'rounded-t-lg',
               index === data.length - 1 && 'border-b rounded-b-lg'
             )}
           >
-            <Text className='text-base'>{toOptions(item)}</Text>
-            <Icon as={ChevronRight} className='text-foreground/50 size-4' />
+            <View className='flex-row items-center gap-4'>
+              <Icon as={item.icon} className='text-muted-foreground size-4' />
+              <Text className='text-base font-normal'>{toOptions(item.name)}</Text>
+            </View>
+            <Icon as={ChevronRight} className='text-muted-foreground size-4 stroke-[1.5px]' />
           </Button>
         </Link>
       )}
