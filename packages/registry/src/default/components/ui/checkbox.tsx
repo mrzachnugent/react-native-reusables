c516900212fade0d838bc50cdcf055c32f0dcb84
Zach Nugent <mrzachnugent@gmail.com>
2025-07-04 23:46:32 -0400

    fix: overflowing checkbox content

--- packages/registry/src/default/components/ui/checkbox.tsx
+++ packages/registry/src/default/components/ui/checkbox.tsx
@@ -22,7 +22,7 @@ function Checkbox({
   return (
     <CheckboxPrimitive.Root
       className={cn(
-        'border-primary h-4 w-4 shrink-0 rounded-sm border',
+        'border-primary h-4 w-4 shrink-0 rounded-sm border overflow-hidden',
         Platform.select({
           web: 'peer focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive transition-shadow outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed cursor-default',
         }),
--- packages/registry/src/new-york/components/ui/checkbox.tsx
+++ packages/registry/src/new-york/components/ui/checkbox.tsx
@@ -22,7 +22,7 @@ function Checkbox({
   return (
     <CheckboxPrimitive.Root
       className={cn(
-        'border-input dark:bg-input/30 size-4 shrink-0 rounded-[4px] border shadow-sm',
+        'border-input dark:bg-input/30 size-4 shrink-0 rounded-[4px] border shadow-sm overflow-hidden',
         Platform.select({
           web: 'peer focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive transition-shadow outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed cursor-default',
           native: 'shadow-black/5',
