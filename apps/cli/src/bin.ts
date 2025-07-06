8ca9971245b03e6a3af225f9f7fd989ae74cc4b2
Zach Nugent <mrzachnugent@gmail.com>
2025-07-06 14:39:37 -0400

    feat(cli): make main file + package json adjustments

--- apps/cli/src/bin.ts
+++ apps/cli/src/bin.ts
@@ -1,11 +1,11 @@
 #!/usr/bin/env node
 
-import * as NodeContext from "@effect/platform-node/NodeContext"
-import * as NodeRuntime from "@effect/platform-node/NodeRuntime"
-import * as Effect from "effect/Effect"
-import { run } from "./Cli.js"
+import * as NodeContext from '@effect/platform-node/NodeContext';
+import * as NodeRuntime from '@effect/platform-node/NodeRuntime';
+import * as Effect from 'effect/Effect';
+import { run } from './main.js';
 
 run(process.argv).pipe(
   Effect.provide(NodeContext.layer),
   NodeRuntime.runMain({ disableErrorReporting: true })
-)
+);
