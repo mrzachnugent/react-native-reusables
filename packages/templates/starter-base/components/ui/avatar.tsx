6945b215d12bee11969d78e3013da708845862af
Zach Nugent <mrzachnugent@gmail.com>
2025-07-15 13:49:33 -0400

    feat(showcase): update to use new components and pass doctor check

--- packages/templates/starter-base/components/ui/avatar.tsx
+++ packages/templates/starter-base/components/ui/avatar.tsx
@@ -1,6 +1,8 @@
+'use client';
+
+import { cn } from '@/lib/utils';
 import * as AvatarPrimitive from '@rn-primitives/avatar';
 import * as React from 'react';
-import { cn } from '@/lib/utils';
 
 function Avatar({
   className,
