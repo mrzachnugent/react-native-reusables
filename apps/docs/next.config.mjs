1815c23d74a1d0fbed819ca92d335de71582f4ec
Zach Nugent <mrzachnugent@gmail.com>
2025-07-21 14:37:30 -0400

    feat(docs): add posthog

--- apps/docs/next.config.mjs
+++ apps/docs/next.config.mjs
@@ -47,6 +47,24 @@ const config = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  async rewrites() {
+    return [
+      {
+        source: '/ingest/static/:path*',
+        destination: 'https://us-assets.i.posthog.com/static/:path*',
+      },
+      {
+        source: '/ingest/:path*',
+        destination: 'https://us.i.posthog.com/:path*',
+      },
+      {
+        source: '/ingest/decide',
+        destination: 'https://us.i.posthog.com/decide',
+      },
+    ];
+  },
+  // Required to support PostHog trailing slash API requests
+  skipTrailingSlashRedirect: true,
 };
 
 export default withMDX(withExpo(config));
