d6f04aa3077f1eede1b19164d0206b6c9d1fe58d
Zach Nugent <mrzachnugent@gmail.com>
2025-07-23 21:19:32 -0400

    fix(textarea): examples

--- packages/registry/src/default/examples/textarea.tsx
+++ packages/registry/src/default/examples/textarea.tsx
@@ -1,5 +1,5 @@
 import { Textarea } from '@/registry/default/components/ui/textarea';
 
 export function TextareaPreview() {
-  return <Textarea placeholder="Type your message here." />;
+  return <Textarea placeholder="Type your message here." className="max-w-md" />;
 }
