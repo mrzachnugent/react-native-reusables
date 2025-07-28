638a927185848de8d9beeecbcbdffed17e52521a
Zach Nugent <mrzachnugent@gmail.com>
2025-07-28 14:07:07 -0400

    feat(cli): add alias for summary

--- apps/cli/src/contexts/cli-options.ts
+++ apps/cli/src/contexts/cli-options.ts
@@ -11,7 +11,7 @@ class CliOptions extends Context.Tag("CommandOptions")<
 
 const cwd = Options.directory("cwd", { exists: "yes" }).pipe(Options.withDefault("."), Options.withAlias("c"))
 const yes = Options.boolean("yes", { aliases: ["y"] })
-const summary = Options.boolean("summary")
+const summary = Options.boolean("summary").pipe(Options.withAlias("s"))
 const overwrite = Options.boolean("overwrite", { aliases: ["o"] })
 const all = Options.boolean("all", { aliases: ["a"] })
 const path = Options.text("path").pipe(Options.withDefault(""), Options.withAlias("p"))
