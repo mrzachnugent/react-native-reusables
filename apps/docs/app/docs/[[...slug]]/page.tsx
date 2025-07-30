c34c550dfc6396c605cbc085b0fab4aaa185de2c
Nikita Krupin <krupin.nikita0110@gmail.com>
2025-07-30 03:22:24 -0400

    foundedlabs icon and logo added

--- apps/docs/app/docs/[[...slug]]/page.tsx
+++ apps/docs/app/docs/[[...slug]]/page.tsx
@@ -164,7 +164,9 @@ export function Footer({ url }: { url: string }) {
 
 function TableOfContentFooter() {
   return (
-    <div className="bg-card dark:bg-fd-muted border-border/70 dark:border-border/0 text-fd-foreground/80 group relative mt-12 flex flex-col gap-2 rounded-lg border p-6 text-sm">
+    <div className="bg-card dark:bg-fd-muted border-border/50 dark:border-border/0 text-fd-foreground/80 group relative mt-12 flex flex-col gap-2 overflow-clip rounded-lg border p-6 text-sm">
+      <div className="bg-fd-foreground/10 dark:bg-fd-foreground/20 border-fd-foreground absolute left-8 top-8 h-64 w-64 -translate-x-1/2 -translate-y-1/2 rounded-full border border-dashed [mask-image:radial-gradient(circle_at_center,red,transparent)]" />
+      <FoundedLabsLogo className="mb-4 size-12" />
       <div className="text-balance text-base font-semibold leading-tight group-hover:underline">
         Want to work with us?
       </div>
@@ -201,4 +203,15 @@ export async function generateMetadata(props: { params: Promise<{ slug?: string[
     title: page.data.title,
     description: page.data.description,
   };
+}
+
+function FoundedLabsLogo({ className }: { className?: string }) {
+  return (
+    <svg className={className} viewBox="0 0 72 72" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M60.959 10.0586C67.7645 16.608 72 25.809 72 36C72 55.8822 55.8822 72 36 72C29.0893 72 22.6347 70.0505 17.1523 66.6748C22.3873 68.4072 28.3726 66.8204 32.0146 62.2598C32.131 62.1141 32.2498 61.9652 32.3701 61.8145C37.6242 55.2343 35.3325 44.4927 37.3457 36.3164C39.1367 29.0433 46.4846 24.5989 53.7578 26.3896C54.63 26.6044 55.4613 26.8995 56.2461 27.2646C57.7227 27.9517 59.5618 27.7619 60.5781 26.4893C64.4722 21.6122 64.4576 14.8744 60.959 10.0586ZM36 0C42.9823 4.07255e-07 49.4989 1.99012 55.0176 5.43066C49.6089 3.26535 43.2037 4.77614 39.3809 9.56348C39.2647 9.70901 39.1466 9.85727 39.0264 10.0078C33.7721 16.5881 36.0659 27.3294 34.0527 35.5059C32.2619 42.7792 24.914 47.2244 17.6406 45.4336C16.7675 45.2186 15.935 44.9232 15.1494 44.5576C13.6727 43.8704 11.8339 44.0603 10.8174 45.333C7.25637 49.793 6.96447 55.8098 9.61621 60.4893C3.65007 54.0645 5.51757e-07 45.4592 0 36C0 16.1178 16.1178 0 36 0Z"
+        fill="currentColor"
+      />
+    </svg>
+  );
 }
