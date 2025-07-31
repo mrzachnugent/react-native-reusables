e8357e8f39fba7de98c09d66ffd8697ee3f15ffe
Zach Nugent <mrzachnugent@gmail.com>
2025-07-30 21:29:07 -0400

    feat(registry): add ui auth blocks

---
+++ packages/registry/src/blocks/sign-up.tsx
@@ -0,0 +1,70 @@
+import { SocialConnections } from '@/registry/blocks/social-connections';
+import { Button } from '@/registry/ui/button';
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/registry/ui/card';
+import { Input } from '@/registry/ui/input';
+import { Label } from '@/registry/ui/label';
+import { Separator } from '@/registry/ui/separator';
+import { Text } from '@/registry/ui/text';
+import * as React from 'react';
+import { Pressable, TextInput, View } from 'react-native';
+
+export function SignUp() {
+  const passwordInputRef = React.useRef<TextInput>(null);
+
+  function onEmailSubmitEditing() {
+    passwordInputRef.current?.focus();
+  }
+
+  return (
+    <View className="gap-6">
+      <Card className="border-border/0 sm:border-border shadow-none sm:shadow-sm sm:shadow-black/5">
+        <CardHeader>
+          <CardTitle className="text-center text-xl sm:text-left">Create your account</CardTitle>
+          <CardDescription className="text-center sm:text-left">
+            Welcome! Please fill in the details to get started.
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="gap-6">
+          <View className="gap-6">
+            <View className="gap-1.5">
+              <Label htmlFor="email">Email</Label>
+              <Input
+                id="email"
+                placeholder="m@example.com"
+                keyboardType="email-address"
+                autoComplete="email"
+                autoCapitalize="none"
+                onSubmitEditing={onEmailSubmitEditing}
+                returnKeyType="next"
+                submitBehavior="submit"
+              />
+            </View>
+            <View className="gap-1.5">
+              <View className="flex-row items-center">
+                <Label htmlFor="password">Password</Label>
+              </View>
+              <Input ref={passwordInputRef} id="password" secureTextEntry returnKeyType="send" />
+            </View>
+            <View className="gap-3">
+              <Button className="w-full">
+                <Text>Continue</Text>
+              </Button>
+            </View>
+          </View>
+          <Text className="text-center text-sm">
+            Already have an account?{' '}
+            <Pressable>
+              <Text className="text-sm underline underline-offset-4">Sign in</Text>
+            </Pressable>
+          </Text>
+          <View className="flex-row items-center">
+            <Separator className="flex-1" />
+            <Text className="text-muted-foreground px-4 text-sm">or</Text>
+            <Separator className="flex-1" />
+          </View>
+          <SocialConnections />
+        </CardContent>
+      </Card>
+    </View>
+  );
+}
---
+++ packages/registry/src/blocks/user-menu.tsx
@@ -0,0 +1,82 @@
+import { cn } from '@/registry/new-york/lib/utils';
+import { Avatar, AvatarFallback, AvatarImage } from '@/registry/ui/avatar';
+import { Button } from '@/registry/ui/button';
+import { Icon } from '@/registry/ui/icon';
+import { Popover, PopoverContent, PopoverTrigger } from '@/registry/ui/popover';
+import { Text } from '@/registry/ui/text';
+import type { TriggerRef } from '@rn-primitives/popover';
+import { LogOutIcon, PlusIcon, SettingsIcon } from 'lucide-react-native';
+import * as React from 'react';
+import { View } from 'react-native';
+
+const USER = {
+  fullName: 'Zach Nugent',
+  initials: 'ZN',
+  imgSrc: { uri: 'https://github.com/mrzachnugent.png' },
+  username: 'mrzachnugent',
+};
+
+export function UserMenu() {
+  const popoverTriggerRef = React.useRef<TriggerRef>(null);
+
+  async function onSignOut() {
+    popoverTriggerRef.current?.close();
+  }
+
+  return (
+    <Popover>
+      <PopoverTrigger asChild ref={popoverTriggerRef}>
+        <Button variant="ghost" size="icon" className="size-8 rounded-full">
+          <UserAvatar />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent align="center" side="bottom" className="w-80 p-0">
+        <View className="border-border gap-3 border-b p-3">
+          <View className="flex-row items-center gap-3">
+            <UserAvatar className="size-10" />
+            <View className="flex-1">
+              <Text className="font-medium leading-5">{USER.fullName}</Text>
+              {USER.fullName?.length ? (
+                <Text className="text-muted-foreground text-sm font-normal leading-4">
+                  {USER.username}
+                </Text>
+              ) : null}
+            </View>
+          </View>
+          <View className="flex-row flex-wrap gap-3 py-0.5">
+            <Button variant="outline" size="sm">
+              <Icon as={SettingsIcon} className="size-4" />
+              <Text>Manage Account</Text>
+            </Button>
+            <Button variant="outline" size="sm" className="flex-1" onPress={onSignOut}>
+              <Icon as={LogOutIcon} className="size-4" />
+              <Text>Sign Out</Text>
+            </Button>
+          </View>
+        </View>
+        <Button
+          variant="ghost"
+          size="lg"
+          className="h-16 justify-start gap-3 rounded-none rounded-b-md px-3 sm:h-14">
+          <View className="size-10 items-center justify-center">
+            <View className="border-border bg-muted/50 size-7 items-center justify-center rounded-full border border-dashed">
+              <Icon as={PlusIcon} className="size-5" />
+            </View>
+          </View>
+          <Text>Add account</Text>
+        </Button>
+      </PopoverContent>
+    </Popover>
+  );
+}
+
+function UserAvatar({ className, ...props }: Omit<React.ComponentProps<typeof Avatar>, 'alt'>) {
+  return (
+    <Avatar alt={`${USER.fullName}'s avatar`} className={cn('size-8', className)} {...props}>
+      <AvatarImage source={USER.imgSrc} />
+      <AvatarFallback>
+        <Text>{USER.initials}</Text>
+      </AvatarFallback>
+    </Avatar>
+  );
+}
