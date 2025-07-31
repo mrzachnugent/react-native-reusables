431a63e5d7d13633fbba64de2e314c0da205563d
Zach Nugent <mrzachnugent@gmail.com>
2025-07-30 21:15:30 -0400

    fix(registry): remove non existing exports from lib

--- packages/registry/src/default/lib/index.ts
+++ packages/registry/src/default/lib/index.ts
@@ -1,2 +1,1 @@
-export * from '@/registry/default/lib/constants';
 export * from '@/registry/default/lib/utils';
--- packages/registry/src/new-york/lib/index.ts
+++ packages/registry/src/new-york/lib/index.ts
@@ -1,2 +1,1 @@
-export * from '@/registry/new-york/lib/constants';
 export * from '@/registry/new-york/lib/utils';
